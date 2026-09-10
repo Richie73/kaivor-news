242b2d3e05a9de26cfc60d22f99d905ac6986ce6
Richie73 <richie1.wsp@gmail.com>
2026-09-10 12:32:33 +0100

    Update sw.js

--- Static/sw.js
+++ Static/sw.js
@@ -6,6 +6,7 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
+// Pass-through fetch to prevent hanging on live data
 self.addEventListener('fetch', (event) => {
     event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
