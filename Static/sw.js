aa8cbdd326ec6474b777c78ea3a89d9a9654f555
Richie73 <richie1.wsp@gmail.com>
2026-09-11 16:39:40 +0100

    Update sw.js

--- Static/sw.js
+++ Static/sw.js
@@ -1,18 +1,26 @@
+const CACHE_NAME = 'kaivor-news-v1';
+const ASSETS = [
+  '/',
+  '/static/logo.png',
+  '/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('kaivor-news-v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/manifest.json'
-            ]);
-        })
-    );
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((cachedResponse) => {
-            return cachedResponse || fetch(e.request);
-        })
-    );
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('/');
+        }
+      });
+    })
+  );
 });
